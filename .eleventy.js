a4a7cef9d195ba35203676efa7f24c9e10e80c3c
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 19:32:22 -0400

    site: local SSH deploy [skip ci]

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+// .eleventy.js  (ROOT ONLY)
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "eleventy_src/css": "css", "eleventy_src/js": "js" });
+  // Put built CSS/JS under /eleventy/... so links will resolve
+  eleventyConfig.addPassthroughCopy({
+    "eleventy_src/css": "eleventy/css",
+    "eleventy_src/js":  "eleventy/js"
+  });
+
   return {
-    dir: { input: "eleventy_src", includes: "_includes", data: "_data", output: "_site" },
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "liquid",
-    templateFormats: ["liquid","md","html"]
+    pathPrefix: "/eleventy/",
+    dir: { input: "eleventy_src", includes: "_includes", data: "_data", output: "_site" }
   };
 };
