bc5a5d6307b87749c0ef7ae5b5d3101e35f7092f
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 13:31:21 -0400

    site: probe deploy

--- app/eleventy/.eleventy.js
+++ app/eleventy/.eleventy.js
@@ -1,7 +1,4 @@
-// .eleventy.js (equivalent, more extensible)
-module.exports = function(eleventyConfig) {
-  return {
-    pathPrefix: "/eleventy/",
-    // dir: { input: ".", includes: "_includes", output: "_site" }, // defaults
-  };
+module.exports = {
+  pathPrefix: "/eleventy/"
 };
+
