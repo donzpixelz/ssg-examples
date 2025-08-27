edf241f19830794eb09a578ad482344bb5e5b19a
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 13:40:19 -0400

    site: probe deploy

---
+++ app/.eleventy.js
@@ -0,0 +1,11 @@
+// .eleventy.js (repo root)
+module.exports = function(eleventyConfig) {
+  return {
+    pathPrefix: "/eleventy/",
+    dir: {
+      input: "app/eleventy",
+      includes: "_includes",
+      output: "app/eleventy/_site"
+    }
+  };
+};
--- app/eleventy/.eleventy.js
+++
@@ -1,7 +0,0 @@
-// .eleventy.js (equivalent, more extensible)
-module.exports = function(eleventyConfig) {
-  return {
-    pathPrefix: "/eleventy/",
-    // dir: { input: ".", includes: "_includes", output: "_site" }, // defaults
-  };
-};
