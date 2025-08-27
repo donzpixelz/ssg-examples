bbaed95f1f165830ce5f7f27249c986fa2f89a84
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 19:29:07 -0400

    site: local SSH deploy [skip ci]

--- app/.eleventy.js
+++ app/.eleventy.js
@@ -1,18 +1,13 @@
-// .eleventy.js
+// .eleventy.js  (ROOT ONLY)
 module.exports = function(eleventyConfig) {
-  // Copy your source assets into the /eleventy/ path in the output
+  // Copy your assets into where the site will look for them (respects your pathPrefix)
   eleventyConfig.addPassthroughCopy({
     "eleventy_src/css": "eleventy/css",
-    "eleventy_src/js": "eleventy/js"
+    "eleventy_src/js":  "eleventy/js"
   });
 
   return {
     pathPrefix: "/eleventy/",
-    dir: {
-      input: "eleventy_src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+    dir: { input: "eleventy_src", includes: "_includes", data: "_data", output: "_site" }
   };
 };
