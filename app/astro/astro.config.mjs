18e90c321f5f9a0833caa148b8be80305c82b0a5
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 13:14:10 -0400

    site: local SSH deploy [skip ci]

--- app/astro/astro.config.mjs
+++ app/astro/astro.config.mjs
@@ -2,3 +2,13 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   base: "/astro/"
 });
+
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // If you serve the site at http://yourdomain/astro/
+  base: '/astro/',
+  // Optional but recommended if you know your public URL:
+  // site: 'http://18.220.33.0',
+});
