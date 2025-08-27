a2f4d3f9e5c27d3d274b2f97858bfb2d83835760
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-27 14:19:58 -0400

    11ty: route build output to app/eleventy; move sources to app/eleventy_src

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,9 @@ module.exports = function(eleventyConfig) {
   return {
     pathPrefix: "/eleventy/",
     dir: {
-      input: "app/eleventy",
+      input: "app/eleventy_src",  // where your content/layouts live now
       includes: "_includes",
-      output: "app/eleventy/_site"
+      output: "app/eleventy"      // <-- BUILT HTML goes here (your deploy already uses this)
     }
   };
 };
