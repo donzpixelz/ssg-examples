da9cdcf17982464dfe8c486c2af80856868843aa
donzpixelz <12188920+donzpixelz@users.noreply.github.com>
2025-08-28 04:31:37 -0400

    site: probe deploy

--- app/eleventy_src/.eleventy.js
+++ app/eleventy_src/.eleventy.js
@@ -1,15 +1,4 @@
 // app/eleventy_src/.eleventy.js
-// Minimal, stable config FROM this folder.
-// Disables Nunjucks, ignores .gitignore, and disables layouts to avoid engine errors.
-module.exports = function(eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-    eleventyConfig.addGlobalData("eleventyComputed", { layout: () => false });
-
-    return {
-        dir: { input: ".", includes: "_includes", data: "_data", output: "_site" },
-        templateFormats: ["liquid", "html", "md"], // no Nunjucks
-        markdownTemplateEngine: "liquid",
-        htmlTemplateEngine: "liquid",
-        pathPrefix: "/eleventy/"
-    };
-};
+// Vanilla config: don't change dirs, engines, or prefixes.
+// Lets Eleventy use its defaults and just write templates it finds.
+module.exports = function () { return {}; };
